6e8158c44844e078a1b48d1d95e9bb66b5cf6271
Mahmoud217TR <faafet.mahmoud@gmail.com>
2021-08-30 20:15:58 +0300

    after vue

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="feed-post">
+
+        <!-- Header Start -->
+        <div class="post-header mb-3">
+            <div class="row align-items-center mb-1">
+                <div class="col-1">
+                    <a href="#"><img class="author-image" :src="authSrc" alt="Author Image"></a>
+                </div>
+                <div class="col-6 pl-0">
+                    <a class='author-name' href="#">{{ authName }}</a>
+                    <p class="mb-1 ml-1 post-time">{{ postTime }}</p>
+                </div>
+            </div>
+            <!-- Tags -->
+            <div class="post-header-tags">
+                <a class="tag" href="#">
+                    sorting 
+                </a>
+                <a class="tag" href="#">
+                    c++ 
+                </a>
+            </div>
+            <hr class="post-hr">
+            <!-- Title -->
+            <div class="post-header-title">
+                {{ postTitle }}
+            </div>
+
+        </div>
+        <!-- Header End -->
+
+        <!-- Body Start -->
+        <div class="post-content">
+            {{ postText }}
+        </div>
+        <!-- Body End -->
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['authSrc','authName','postTime','postTitle','postText'],
+        mounted() {
+            console.log('Component mounted.')
+        },
+        
+    }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,44 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-3">
-        <div class="card">
-            <div class="card-header">Notifications</div>
-            <div class="card-body">
-                <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam quod impedit illum soluta corrupti cupiditate id praesentium a quia, voluptatem ea excepturi hic voluptas omnis deserunt quam asperiores harum culpa facilis quae consequatur minima, recusandae sed. Sunt nihil blanditiis omnis, quam similique expedita aperiam suscipit, sit quae facere, minus nesciunt qui earum? Aperiam sapiente reiciendis soluta veniam ipsum suscipit at laboriosam ad quidem. Quia odit dignissimos architecto optio delectus quae accusantium explicabo asperiores vero. Exercitationem totam molestias molestiae ipsum, ducimus blanditiis dolorem quibusdam iste dicta aliquid quam nesciunt. Vel veniam ex possimus id, aperiam cumque rerum praesentium non consectetur consequatur.
-                </p>
-            </div>
-        </div>
-    </div>
-    <div class="col-8">
-
-        <div class="feed">
-            <div class="feed-post">
-
-                <div class="post-header mb-3">
-                    <div class="row align-items-center mb-1">
-                        <div class="col-1">
-                            <a href="#"><img class="author-image" src="{{ asset('img/admin.png') }}" alt="Author Image"></a>
-                        </div>
-                        <div class="col-6 pl-0">
-                            <a class='author-name' href="#">Author name</a>
-                            <p class="mb-1 ml-1 post-time">30-Aug-2021 6:00PM</p>
-                        </div>
-                    </div>
-
-                    <div class="post-header-title">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, eaque.
-                    </div>
-                </div>
-
-                <div class="post-content">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt illo repellat exercitationem doloremque sapiente porro aliquam repellendus placeat asperiores impedit commodi, quas voluptatum rem praesentium blanditiis saepe soluta aspernatur ratione a? Consequuntur cum eos facere obcaecati rerum iste vel! Minima.
+<div class="container home-container">
+    <div class="row justify-content-center">
+        <div class="col-3">
+            <div class="card">
+                <div class="card-header">Notifications</div>
+                <div class="card-body">
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam quod impedit illum soluta corrupti cupiditate id praesentium a quia, voluptatem ea excepturi hic voluptas omnis deserunt quam asperiores harum culpa facilis quae consequatur minima, recusandae sed. Sunt nihil blanditiis omnis, quam similique expedita aperiam suscipit, sit quae facere, minus nesciunt qui earum? Aperiam sapiente reiciendis soluta veniam ipsum suscipit at laboriosam ad quidem. Quia odit dignissimos architecto optio delectus quae accusantium explicabo asperiores vero. Exercitationem totam molestias molestiae ipsum, ducimus blanditiis dolorem quibusdam iste dicta aliquid quam nesciunt. Vel veniam ex possimus id, aperiam cumque rerum praesentium non consectetur consequatur.
+                    </p>
                 </div>
             </div>
         </div>
-
+        <div class="col-7">
+            <div class="feed">
+                <post-component auth-src="{{ asset('img/admin.png') }}" auth-name = "Author Name" post-time ="30-Aug-2021 6:00PM"
+                                post-title = "This is a long post title for testing only"
+                                post-text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt illo repellat exercitationem doloremque sapiente porro aliquam repellendus placeat asperiores impedit commodi, quas voluptatum rem praesentium blanditiis saepe soluta aspernatur ratione a? Consequuntur cum eos facere obcaecati rerum iste vel! Minima."
+                ></post-component>
+                <post-component auth-src="{{ asset('img/admin.png') }}" auth-name = "Author Name" post-time ="30-Aug-2021 6:00PM"
+                                post-title = "This is a long post title for testing only"
+                                post-text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt illo repellat exercitationem doloremque sapiente porro aliquam repellendus placeat asperiores impedit commodi, quas voluptatum rem praesentium blanditiis saepe soluta aspernatur ratione a? Consequuntur cum eos facere obcaecati rerum iste vel! Minima."
+                ></post-component>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
